7444344b99321e7d771eb9733c2063982fdd2fc7
shanelle-hub <njingtishanelle103@gmail.com>
2024-07-19 13:28:31 +0100

    responsive partner button

--- src/components/Agenda.jsx
+++ src/components/Agenda.jsx
@@ -5,6 +5,11 @@ function Agenda() {
   return (
     <div>
       <p>
+      Open Dreams is a non-profit organisation dedicated to providing equal access to educational opportunities for  high achieving students from low income background. This is achieved through establishing educational centers, offering information on scholarships, and providing mentorship programs. 
+
+This year, Open Dreams is poised to mark a decade of transforming lives and shaping futures. The highly anticipated Open Dreams 10th Anniversary Celebration is set to take place from *July 25th to July 27th, 2024.* The festivities will kick off with an exhilarating hike on July 20th, and end with a thanksgiving service on July 28th, 2024, marking a decade of triumph. 
+
+The upcoming anniversary promises to be an unforgettable celebration of excellence, impact, and collaboration, bringing together scholars, program facilitators, mentors, volunteers, donors, educational stakeholders, and supporters in a grand showcase of success and inspiration
       </p>
     </div>
   );
